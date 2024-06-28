f414138c70a98631729bd18fc9e3d49b725fffaf
Murilo Oliveira <murilodioliveira@gmail.com>
2024-06-27 23:02:43 -0300

    Tentando puxar dados da página teste

---
+++ web_scra.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+
+# Acessando página mais simples para extrapolação das outras
+URL = "https://realpython.github.io/fake-jobs/" 
+pagina = requests.get(URL) 
+
+# Teste com página real
+URL2 = "https://www.washingtonpost.com/technology/2020/09/25/privacy-check-blacklight/"
+#pagina2 = requests.get(URL2)
+
+
+#print(pagina.status_code) # Primeira conexão - OK (200)
+#print(pagina2.status_code)# Conexão site real - OK (200)
+
+soup = BeautifulSoup(pagina.content, "html.parser")
+
+resultado = soup.find_all("div",class_="subtitle is-3")
+print(resultado.prettify())
